a689a6ce6b750477ff7b0c1d23db0ccd2279049d
armass007 <52897342+armass007@users.noreply.github.com>
2019-08-11 18:42:37 -0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,14 +28,12 @@ If you're enjoying using me, and/or would like to help me survive in the wild, s
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
+I'm a group management bot created by Arvind. 
 
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
