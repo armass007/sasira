4a4a22fa1d7226aa2f0b00a7d77d0cb0a3ab0399
armass007 <52897342+armass007@users.noreply.github.com>
2019-08-10 21:49:39 -0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,18 +19,11 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @i_m_arvind.
-
-I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
-python-telegram-bot library, and am fully [opensource]
-
-Feel free to submit pull requests on github, or to contact my support group, @i_am_arvind, with any bugs, questions \
-or feature requests you might have :)
-Contact me @i_am_arvind for announcements on new features, downtime, etc.
+Hi {}, my name is {}! If you have any questions on how to use me find me @Telugu or @Tamil or @Hindi.
 
 You can find the list of available commands with /help.
 
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+If you're enjoying using me, and/or would like to help me survive in the wild, send me thank you note!
 """
 
 HELP_STRINGS = """
